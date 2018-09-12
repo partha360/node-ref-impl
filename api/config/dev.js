ce33dfe5d678ca8be087cae8c191716f7c9acfe5
Parthasarathy S <partha@finiteloop.io>
2018-09-12 18:28:13 +0530

    Add local mongo container

--- api/config/dev.js
+++ api/config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID: '345016066786-e2qlg577lrflsdd8jo1rqsri0fg5h82f.apps.googleusercontent.com',
   googleClientSecret: '_VbpwWerzIwlTF9oxeJrlGsy',
-  mongoURI:
-    'mongodb://partha360:calvin51@parthacluster-shard-00-00-six5v.mongodb.net:27017,parthacluster-shard-00-01-six5v.mongodb.net:27017,parthacluster-shard-00-02-six5v.mongodb.net:27017/blogs?ssl=true&replicaSet=ParthaCluster-shard-0&authSource=admin',
+  // mongoURI:
+  //   'mongodb://partha360:calvin51@parthacluster-shard-00-00-six5v.mongodb.net:27017,parthacluster-shard-00-01-six5v.mongodb.net:27017,parthacluster-shard-00-02-six5v.mongodb.net:27017/blogs?ssl=true&replicaSet=ParthaCluster-shard-0&authSource=admin',
+  mongoURI: 'mongodb://db:27017/blogs',
   cookieKey: '123123123'
 }
